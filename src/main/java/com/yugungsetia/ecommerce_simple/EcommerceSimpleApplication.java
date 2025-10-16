550839b113e120f1a73308076ef0a87b326c7401
agungsetiawan031202 <agung.joko@iconpln.co.id>
2025-10-16 15:01:57 +0700

    initial commit

---
+++ src/main/java/com/yugungsetia/ecommerce_simple/EcommerceSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.yugungsetia.ecommerce_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSimpleApplication.class, args);
+	}
+
+}
